d1f10c951f51c4e08ebce115440a706d365d97e4
omercsx <omercagris@gmail.com>
2024-09-13 19:13:46 -0400

    open ended question boilerplate

---
+++ src/components/OpenEndedQuestion/OpenEndedQuestion.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const OpenEndedQuestion = () => {
+  return (
+    <View>
+      <Text>OpenEndedQuestion</Text>
+    </View>
+  )
+}
+
+export default OpenEndedQuestion
