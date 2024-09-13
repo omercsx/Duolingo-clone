5479e8a7ec4b305af3277db1e7bcff24322f6e31
omercsx <omercagris@gmail.com>
2024-09-13 13:26:52 -0400

    quiz

--- App.js
+++ App.js
@@ -1,24 +1,43 @@
-import { useState } from 'react'
-import { Text, View } from "react-native";
+import { useState, useEffect } from 'react'
+import { Alert, Text, View } from "react-native";
 
 import styles from "./App.styles"
-import oneQuestionWithOption from "./assets/data/oneQuestionWithOption"
 import ImageOption from "./src/components/ImageOption"
 import Button from "./src/components/Button"
+import questions from "./assets/data/imageMulatipleChoiceQuestions"
 
 const App = () => {
   const [selected, setSelected] = useState(null)
-  const { question, options } = oneQuestionWithOption
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(questions[currentQuestionIndex])
+
+
+  useEffect(() => {
+    if (currentQuestionIndex >= questions.length) {
+      Alert.alert("You've reached the end of the game! You won! Restarting game...")
+      setCurrentQuestionIndex(0)
+    } else {
+      setCurrentQuestion(questions[currentQuestionIndex])
+    }
+  }, [currentQuestionIndex])
 
   const onButtonPress = () => {
-    console.log("Button pressed")
+    if (selected.correct) {
+
+      // move to next question
+      setCurrentQuestionIndex(currentQuestionIndex + 1)
+      setSelected(null)
+
+    } else {
+      Alert.alert("Wrong")
+    }
   }
 
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>{question}</Text>
+      <Text style={styles.title}>{currentQuestion.question}</Text>
       <View style={styles.optionsContainer}>
-        {options.map(option => {
+        {currentQuestion.options.map(option => {
           return <ImageOption
             key={option.id}
             image={option.image}
